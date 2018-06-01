bdf86b2e562dd8b5d751c9334b6f9b11ebe530cb
kroyo <kroyoshi@163.com>
2018-06-01 19:14:49 +0800

    vue-todo

---
+++ src/components/layouts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <aside class="menu">
+      <menus></menus>
+    </aside>
+    <article class="content-container">
+      <todo></todo>
+    </article>
+  </div>
+</template>
+<script>
+  import menus from './menus';
+  import todo from './todo';
+  export default { 
+    components: {
+      menus,
+      todo
+    }
+  };
+</script>
+<style lang="less">
+@import '../common/style/layouts.less';   
+</style>
---
+++ src/components/menus.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list-todos">
+    <a class="list-todo list">
+      <span class="icon-lock"></span>
+      <span class="count-list">1</span>
+      星期一
+    </a>
+    <a class="link-list-new">
+      <span class="icon-plus"></span>
+      新增
+    </a>
+  </div> 
+</template>
+<script>
+ export default {  };   
+</script>
+<style lang="less">
+@import '../common/style/menu.less';    
+</style>
